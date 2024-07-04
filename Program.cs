a41f39fb58022f8d16e4b5417512a5c9692e36fa
SkillFactoryCSharpTest <dashulichka311214@gmail.com>
2024-07-04 18:15:33 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Практ9._6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Exception[] exceptions = new Exception[] 
+            {
+                new IndexOutOfRangeException(),
+                new MyOwnException("ошибочка"),
+                new ArgumentOutOfRangeException(),
+                new KeyNotFoundException(),
+                new InvalidOperationException()
+            };
+            
+                foreach (var exception in exceptions) 
+                {
+                    try 
+                    {
+                        throw exception;
+                    }
+                    catch (Exception ex) 
+                    {
+                        Console.WriteLine(ex.Message);
+                        Console.ReadKey();
+                    }
+
+                }
+            
+        }
+    }
+    public class MyOwnException : Exception
+    {
+        public MyOwnException (string message): base (message) { }
+    }
+}
